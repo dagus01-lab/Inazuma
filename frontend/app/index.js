39fc6e276aac68b35d2655601435ace0470b3af2
kiaia69 <francesco.chiaia5@gmail.com>
2023-09-10 22:36:48 +0200

    THIRD

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -2,17 +2,25 @@ import { useState } from 'react';
 import { View, Text, ScrollView  } from 'react-native';
 import { Stack, useRouter } from 'expo-router';
 
-import Login from '../components/login/Login'
+import Registrazione from '../components/registrazione/Registrazione'
+import Login from '../components/login/Login';
 
 const Home = () => {
     const router = useRouter();
     const [isLogged, setIsLogged] = useState(false);
 
+    const logIn = (email, password) => {
+        if(true){
+            setIsLogged(true);
+        } // controllo delle credenziali
+      
+    }
+
     return (
-        <View style={{  backgroundColor: 'white' , width: "100%", flex: 1}}>
+        <View style={{  backgroundColor: '#f4f4f4' , width: "100%", flex: 1}}>
              <Stack.Screen
             options={{
-                // headerStyle: { backgroundColor: COLORS.lightWhite },
+                 headerStyle: { backgroundColor: '#f4f4f4'},
                  headerShadowVisible: false,
                 // headerLeft: () => (
                 //     <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"/>
@@ -20,11 +28,11 @@ const Home = () => {
                 // headerRight: () => (
                 //     <ScreenHeaderBtn iconUrl={images.profile} dimension="100%"/>
                 // ),
-                headerTitle: "",
+                 headerTitle: "",
             }}/>
             <ScrollView showsVerticalScrollIndicator={false} style={{width: "100%"}}>
-            {/* { isLogged ? ( <Login /> ) : ( <Login/> ) } */}
-            <Login></Login>
+            {/* { isLogged ? ( <Home /> ) : ( <Login/> ) } */}
+            <Login onLogIn={logIn}></Login>
             </ScrollView>
         </View>
     )
--- frontend/components/login/Login.js
+++ frontend/components/login/Login.js
@@ -1,49 +1,25 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Pressable, TouchableOpacity, Platform} from 'react-native';
+import { View, Text, TextInput, Pressable, TouchableOpacity} from 'react-native';
 import { useRouter } from 'expo-router';
 import styles from './Login.style';
-import BouncyCheckbox from "react-native-bouncy-checkbox";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { useTogglePasswordVisibility } from '../utili/useTogglePasswordVisibility';
-import { useTogglePasswordVisibility_other } from '../utili/useTogglePasswordVisibility_other';
-import DateTimePicker from '@react-native-community/datetimepicker';
 
-const Login = () => {
+const Login = (onLogIn) => {
   const { passwordVisibility, rightIcon, handlePasswordVisibility } = useTogglePasswordVisibility();
-  const { passwordVisibility_other, rightIcon_other, handlePasswordVisibility_other } = useTogglePasswordVisibility_other();
   const router = useRouter();
-  const [nomeCognome, setNomeCognome] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [confPass, setConfPass] = useState('');
-  const [cellulare, setCellulare] = useState(null);
-  const [date, setDate] = useState(new Date());
-  const [date_string, setDateString] = useState('');
-  const [showDatePicker, setShowDatePicker] = useState(false);
 
-  const toggleDatePicker = () => {
-    setShowDatePicker(!showDatePicker);
-  };
+  const recuperaPassword = () => {
+    //funzione da implementare
+    alert("Funzione ancora non implementata")
+  }
 
-  const onChange = (event, selectedDate) => {
-    if (selectedDate) {
-      // Verifica se selectedDate è definito (l'utente ha effettivamente selezionato una data)
-      const currentDate = selectedDate || date;
-      setDate(currentDate);
-      toggleDatePicker();
-      const dateString = currentDate.toDateString(); // Converte la data in una stringa
-      setDateString(dateString);
-    } else {
-      // L'utente ha annullato la selezione, puoi gestire questo caso se necessario
-      toggleDatePicker();
-      }
-
+  const registrazione = () => {
+    router.push('');
   }
   
-  const creaAccount = () => {
-    alert(date_string);
-  }
-
   return (
     <View style={styles.container}> 
      <View style={styles.riga}>
@@ -55,53 +31,37 @@ const Login = () => {
       </View>
       <View>
       <View style={styles.textInput}>
-        <TextInput style={styles.input} value={nomeCognome} onChangeText={(text) => {setNomeCognome(text)}} placeholder="Nome e Cognome" placeholderTextColor="#d3d3d3"/>
-      </View>
-      <View style={styles.textInput}>
-        <TextInput style={styles.input} value={email} onChangeText={(text) => {setEmail(text)}} placeholder="Email" placeholderTextColor="#d3d3d3"/>
+        <TextInput style={styles.inputField} value={email} onChangeText={(text) => {setEmail(text)}} placeholder="   Email" placeholderTextColor="#d3d3d3"/>
       </View>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.inputField} name="password" placeholder="Password" autoCapitalize="none" autoCorrect={false} textContentType="newPassword" secureTextEntry={passwordVisibility} value={password} enablesReturnKeyAutomatically onChangeText={text => setPassword(text)} placeholderTextColor="#d3d3d3"/>
-        <Pressable onPress={handlePasswordVisibility}>
+        <TextInput style={styles.inputField} name="password" placeholder="   Password" autoCapitalize="none" autoCorrect={false} textContentType="newPassword" secureTextEntry={passwordVisibility} value={password} enablesReturnKeyAutomatically onChangeText={text => setPassword(text)} placeholderTextColor="#d3d3d3"/>
+        <Pressable style={{marginRight: 10}} onPress={handlePasswordVisibility}>
           <MaterialCommunityIcons name={rightIcon} size={22} color="#232323" />
         </Pressable>
       </View>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.inputField} name="confermaPassword" placeholder="Conferma password" autoCapitalize="none" autoCorrect={false} textContentType="newPassword" secureTextEntry={passwordVisibility_other} value={confPass} enablesReturnKeyAutomatically onChangeText={text => setConfPass(text)} placeholderTextColor="#d3d3d3"/>
-        <Pressable onPress={handlePasswordVisibility_other}>
-          <MaterialCommunityIcons name={rightIcon_other} size={22} color="#232323" />
-        </Pressable>
-      </View>
-      <View style={styles.textInput}>
-        <TextInput style={styles.input} value={cellulare} onChangeText={(text) => {setCellulare(text)}} placeholder="Cellulare" keyboardType="numeric" placeholderTextColor="#d3d3d3"/>
-      </View>
-      { !showDatePicker && (
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.inputField} value={date_string} placeholder="Data di nascita" editable={false} placeholderTextColor="#d3d3d3"/>
-        <Pressable onPress={toggleDatePicker}>
-        <MaterialCommunityIcons name="calendar-blank" size={30} color="#000"/>
-        </Pressable>
-      </View>
-      ) }
-      { showDatePicker && (
-        <View style={styles.date_container}>
-        <Text style={{marginTop: 10}}>Seleziona la tua data di nascita:</Text>
-        <DateTimePicker style={styles.date} testID="dateTimePicker"  value={date} mode="date" is24Hour={true} onChange={onChange}/>
-        </View>
-      ) 
-      }
-      <View>
-      <View style={styles.box}>
-        <BouncyCheckbox style={{marginTop: -3}} fillColor='#1e90ff'></BouncyCheckbox><Text>Accetto di ricevere notifiche via SMS/Mail</Text>
-      </View>
-      <View style={styles.boxDue}>
-        <BouncyCheckbox style={{marginTop: -3}}  fillColor='#1e90ff'></BouncyCheckbox><Text>Accetto i termini e le condizioni</Text>
-      </View>
-      </View>
-      <TouchableOpacity style={styles.button} onPress={creaAccount} >
-         <Text style={styles.buttonText}>CREA ACCOUNT</Text>
+      <TouchableOpacity style={styles.button} onPress={ () => onLogIn(email, password)} >
+         <Text style={styles.buttonText}>Login</Text>
       </TouchableOpacity>
       </View>
+      <View>
+        <Pressable onPress={recuperaPassword}>
+        <Text style={{color: '#1e90ff', textDecorationLine:'underline', marginTop: 38}}>Hai dimenticato la password?</Text>
+        </Pressable>
+      </View>
+      <View style={styles.horizontalLine}>
+          <View style={styles.line} />
+          <Text style={styles.lineText}>oppure</Text>
+          <View style={styles.line} />
+        </View>
+        {/* //inserire la possibilita di loggarsi con google e facebook */}
+        <View>
+          <Text>Se non hai un account</Text>
+        </View>
+        <View>
+        <Pressable onPress={registrazione}>
+          <Text style={{color: '#1e90ff', textDecorationLine:'underline'}}>Registrati qui!</Text>
+          </Pressable>
+        </View>
 
     </View>
   );
